aab3528dbd408803b5cc5c34325d41b4b9deaab3
steve nash <stefanospapadam@gmail.com>
2021-10-15 11:51:54 +0300

    Documents interface.py module

--- interface.py
+++ interface.py
@@ -10,31 +10,55 @@ from communicationMQTT import   VehicleSubscriberCoorMQTT, \
 from town import Town
 
 class Interface(object):
+    """
+    Description:
+        Class Interface implements the logic of choosing correct locations or directions by the user 
+
+    """    
+
     def __init__(self, world, map, vehicle_actor):
+        """
+        Description:
+            Method __init__ is the Constructor of Class Interface that initializes most of the used variables 
+
+        Args:
+            world           (carla.World)           :    World object of CARLA API
+            map             (carla.Map)             :    Map object of CARLA API
+            vehicle_actor   (carla.Vehicle)         :    The actor object of the autonomous vehicle
+        """        
+
         self.world          = world
         self.map            = map
         self.vehicle_actor  = vehicle_actor
         self.cancel         = Cancel()
         self.trajectory     = Trajectory(world, map, vehicle_actor)
 
-        self.pub_waypoint       = VehiclePublisherMQTT(topic='waypoint_choose')
-        self.pub                = VehiclePublisherMQTT(topic='clean')
-        self.pub_vel            = VehiclePublisherMQTT(topic='speed_topic')
-        self.pub_vel_conf       = VehiclePublisherMQTT(topic='speed_configure')
-        self.pub_forward        = VehiclePublisherMQTT(topic='forward meters')
-        self.pub_turn           = VehiclePublisherMQTT(topic='turn number')
-        self.pub_turn_clean     = VehiclePublisherMQTT(topic='turn clean')
+        self.pub_waypoint       = VehiclePublisherMQTT(topic='waypoint_choose'  )
+        self.pub                = VehiclePublisherMQTT(topic='clean'            )
+        self.pub_vel            = VehiclePublisherMQTT(topic='speed_topic'      )
+        self.pub_vel_conf       = VehiclePublisherMQTT(topic='speed_configure'  )
+        self.pub_forward        = VehiclePublisherMQTT(topic='forward meters'   )
+        self.pub_turn           = VehiclePublisherMQTT(topic='turn number'      )
+        self.pub_turn_clean     = VehiclePublisherMQTT(topic='turn clean'       )
 
-        self.sub_enter          = VehicleSubscriberEnterMQTT(topic='enter')
-        self.sub_done           = VehicleSubscriberDoneMQTT(topic='done')
-        self.sub_coor           = VehicleSubscriberCoorMQTT(topic='coordinates')
-        self.sub_coor_forward   = VehicleSubscriberCoorForwardMQTT(topic='coordinates_forward')
-        self.sub_forward        = VehicleSubscriberForwardMQTT(topic='forward')
+        self.sub_enter          = VehicleSubscriberEnterMQTT        (topic='enter'              )
+        self.sub_done           = VehicleSubscriberDoneMQTT         (topic='done'               )
+        self.sub_coor           = VehicleSubscriberCoorMQTT         (topic='coordinates'        )
+        self.sub_coor_forward   = VehicleSubscriberCoorForwardMQTT  (topic='coordinates_forward') 
+        self.sub_forward        = VehicleSubscriberForwardMQTT      (topic='forward'            )
 
-    #############################
-    # Handle location decisions #
-    #############################
+
     def handle(self, start_waypoint):
+        """
+        Description:
+            Method handle is used to handle location decisions by adding the location of the chosen destination
+
+        Args:
+            start_waypoint (carla.Waypoint): The start waypoint from which this segment of the path will be calculated 
+
+        Returns:
+            list: A list of end waypoints that should be passed by the vehicle
+        """        
 
         msg = {'value': ''}
         self.pub_waypoint.publish(msg)
@@ -55,11 +79,12 @@ class Interface(object):
 
         while True:
             self.world.tick()
+
             if self.cancel.cancel_process():
                 self.cancel.cancel_now(False)
                 break
 
-                # Waiting for ENTER button to be pressed
+            # Waiting for ENTER button to be pressed
             if self.sub_enter.get_enter() == True:
 
                 self.sub_enter.set_enter(False)
@@ -93,10 +118,18 @@ class Interface(object):
                 break
         return end_waypoints
 
-    ###########################
-    # Handle forward decision #
-    ###########################
+
     def handle_forward(self, start_waypoint):
+        """
+        Description:
+            Method handle_forward is used to handle forward decisions by adding to the path as many meters have been logged by the user
+
+        Args:
+            start_waypoint (carla.Waypoint): The start waypoint from which this segment of the path will be calculated 
+
+        Returns:
+            list: A list of waypoints that were added in the overall trajectory and should be passed by the vehicle
+        """        
 
         waypoints = []
         waypoint = start_waypoint
@@ -152,11 +185,20 @@ class Interface(object):
         '''  
         return waypoints
 
-    ########################
-    # Handle turn decision #
-    ########################
+
     def handle_turn(self, start_waypoint, turn):
-      
+        """
+        Description:
+            Method handle_turn is used to handle turn decisions by adding to the path the direction's waypoints that have been chosen by the user  
+
+        Args:
+            start_waypoint (carla.Waypoint): The start waypoint from which this segment of the path will be calculated 
+            turn (str): The turn direction
+
+        Returns:
+            list: A list of waypoints that were added in the overall trajectory and should be passed by the vehicle
+        """   
+
         waypoints = [start_waypoint]
         paths = waypoints[len(waypoints) - 1].next(1.0)
 
@@ -181,6 +223,7 @@ class Interface(object):
         except:
             pass
 
+        # for each available path 
         for i in range(len(paths)):
 
             ways = paths[i].next_until_lane_end(1.0)
@@ -188,22 +231,26 @@ class Interface(object):
               
             angle = calculate_angle(ways)
 
+            # change the coordinate system in order to compare the starting and the final location
             final_point = change_coordinate_system(paths[i].transform, ways[len(ways) - 1].transform.location)
             initial_point = change_coordinate_system(ways[0].transform, ways[0].transform.location)
 
+            # case of STRAIGHT is detected by the calculated angle between the starting and the final waypoint 
             if round(angle, 1) == 0.8 and turn == "STRAIGHT":
-
-                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False,
-                                             color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
+                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False, color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
                 self.pub_waypoint.publish({'value': 'Going STRAIGHT at the next junction'})
                 self.pub.publish({'value': ''})
+                
+                # add waypoints to the path 
                 for i in range(len(ways)):
                     waypoints.append(ways[i])
                 break
 
+            # case of RIGHT is detected by the new coordinates 
             elif ((final_point.x < 0 and final_point.y < 0) or (
                     final_point.x > 0 and final_point.y > 0)) and turn == "RIGHT" and round(angle, 1) != 0.8:
 
+                # if more than one RIGHT turns exist
                 if self.right_counter > 1:
                     self.pub_turn.publish({'value': "Specify the number of turn you want to follow!"})
                     while True:
@@ -240,15 +287,16 @@ class Interface(object):
                     for i in range(len(ways)):
                         waypoints.append(ways[i])
 
-                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False,
-                                             color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
+                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False, color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
                 self.pub_waypoint.publish({'value': 'Turn RIGHT at the next junction'})
                 self.pub.publish({'value': ''})
                 break
 
+            # case of LEFT is detected by the new coordinates 
             elif ((final_point.x > 0 and final_point.y < 0) or (
                     final_point.x < 0 and final_point.y > 0)) and turn == "LEFT" and round(angle, 1) != 0.8:
 
+                # if more than one LEFT turns exist
                 if self.left_counter > 1:
                     self.pub_turn.publish({'value': "Specify the number of turn you want to follow!"})
 
@@ -287,8 +335,7 @@ class Interface(object):
                     for i in range(len(ways)):
                         waypoints.append(ways[i])
 
-                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False,
-                                             color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
+                self.world.debug.draw_string(ways[len(ways) - 1].transform.location, "XXXXXXX", draw_shadow=False, color=carla.Color(r=0, g=250, b=0), life_time=1000, persistent_lines=True)
                 self.pub_waypoint.publish({'value': 'Turn LEFT at the next junction'})
                 self.pub.publish({'value': ''})
                 break
@@ -300,13 +347,16 @@ class Interface(object):
 
         return waypoints
 
-    ####################################################
-    # Function for informing about possible directions #
-    ####################################################
-    def turn_info(self, start_waypoint):
 
-        # This try except block is used in case of the start waypoint is at the end of the lane and it cannot produse new waypoints. 
-        # Therefore it throws a RuntimeError and we use as waypoints the starting waypoint  
+    def turn_info(self, start_waypoint):
+        """
+        Description:
+            Method turn_info is used to provide information to the user about the available turns that could be followed 
+
+        Args:
+            start_waypoint (carla.Waypoint): The start waypoint from which this segment of the path will be calculated 
+        """
+
         waypoints = [start_waypoint]
         paths = waypoints[len(waypoints) - 1].next(1.0)
 
@@ -364,10 +414,16 @@ class Interface(object):
         # publish result 
         self.pub_waypoint.publish({'value': self.turn})
 
-    #############################################
-    # Function for checking possible directions #
-    #############################################
+
     def check_directions(self, paths, check_duplicates):
+        """
+        Description:
+            Method check_directions is used as a helper function in order to check the available paths that could be followed 
+
+        Args:
+            paths               (list)      :   A list with the available starting waypoints for each path 
+            check_duplicates    (boolean)   :   A boolean value that is True when it is needed to check for more than one turns in the same direction 
+        """
 
         for i in range(len(paths)):
 
@@ -378,7 +434,7 @@ class Interface(object):
             angle = calculate_angle(ways)
 
             # convert coordinates to a coordinate system taking as starting point, the starting point of the path.
-            # this is done in order to decide in which direction is each path located.  
+            # this is done in order to decide in which direction each path is located.  
             final_point = change_coordinate_system(paths[i].transform, ways[len(ways) - 1].transform.location)
             initial_point = change_coordinate_system(ways[0].transform, ways[0].transform.location)
 
@@ -420,4 +476,3 @@ class Interface(object):
                 self.turn_draw_left = "LEFT {}".format(self.left_counter)
 
                 self.world.debug.draw_string(ways[len(ways) - 1].transform.location, self.turn_draw_left, draw_shadow=False, color=carla.Color(r=0, g=0, b=250), life_time=1000, persistent_lines=True)
-                
