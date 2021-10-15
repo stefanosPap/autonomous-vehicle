05b9482d5e69cf005cf6d8cbd43dcf17ee377a9f
steve nash <stefanospapadam@gmail.com>
2021-10-15 16:05:37 +0300

    Documents town.py module

--- town.py
+++ town.py
@@ -1,8 +1,18 @@
 import carla 
 
 class Town(object):
-    
+    """
+    Description:
+        Class Town is used to specify the characteristics of each final location 
+
+    """    
     def __init__(self, world):
+        """
+            Method __init__ is the Constructor of Class Town that initializes the coordinates of each location
+
+        Args:
+            world   (carla.World)    :  World object of CARLA API
+        """
         self.destinations = {"Gas Station"          : [ -33,  176,  0], \
                              "Central Roundabout"   : [   0,   20,  0], \
                              "Circular Plaza"       : [  97,   57,  0], \
@@ -23,5 +33,14 @@ class Town(object):
             gs = carla.Location(self.destinations[key][0], self.destinations[key][1], self.destinations[key][2])
             world.debug.draw_string(gs, key, draw_shadow=False, color=carla.Color(r=0, g=0, b=0), life_time=1000, persistent_lines=True)
 
+
     def get_destinations(self):
+        """
+        Description:
+            Method get_destinations is used to return the dictionary with the available destinations
+
+        Returns:
+            dictionary: A dictionary with keys the names of destinations and values the coordinates of the destinations
+        """    
+
         return self.destinations
