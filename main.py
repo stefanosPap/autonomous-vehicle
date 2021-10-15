64ea1d4d728986017ea38a960599437600fecb17
steve nash <stefanospapadam@gmail.com>
2021-10-15 12:29:17 +0300

    Documents main.py module

--- main.py
+++ main.py
@@ -40,8 +40,14 @@ except IndexError:
     pass
 
 def main():
+    """
+    Description:
+        Function main is the central function from which the whole process is starting
+    """    
 
-    # create client # 
+    #
+    # Create client  
+    #
     client = Client()
     client.connect()  # connect the client
     [blueprint, world, map] = client.get_simulation()
@@ -49,36 +55,49 @@ def main():
     vehicle_list = []
     walker_list  = []
 
+    #
     # Specify Town in CARLA #
+    #
     # world = client.get_client().load_world('Town02')
     # world = client.get_client().reload_world()
     # print(client.get_client().get_available_maps())
     
-    points = map.get_spawn_points()  # returns a list of recommendations
+    #
+    # case of spawning the ego vehicle at a random place     
+    #
+    points = map.get_spawn_points()                                                     # returns a list of recommendations
     while True:
-        start_point = random.choice(points)  # choose first point as spawn point
+        start_point = random.choice(points)                                             # choose first point as spawn point
         start_waypoint = map.get_waypoint(start_point.location, project_to_road=False)  # return the waypoint of the spawn point
-        if start_waypoint.get_junction() == None:  # spawn vehicles only on roads, not junctions
+        if start_waypoint.get_junction() == None:                                       # spawn vehicles only on roads, not junctions
             break
 
+    #
+    # case of spawning the ego vehicle at a specific place     
+    #
     start_point = carla.Transform(carla.Location(0, 20,  1), carla.Rotation(pitch=0.0, yaw=0.0, roll=0.0))
     start_waypoint = map.get_waypoint(start_point.location, project_to_road=True)
     
-    # create new ego vehicle #
+    #
+    # create new ego vehicle 
+    #
     vehicle = Vehicle()
     vehicle.choose_spawn_point(start_point)              # spawn the vehicle
     vehicle.choose_model('model3', blueprint, world)     # choose the model
     vehicle_actor = vehicle.get_vehicle_actor()          # return actor object of the vehicle
     vehicle_transform = vehicle.get_vehicle_transform()  # get vehicle's transform
 
-    vehicle_list.append(vehicle_actor)
+    vehicle_list.append(vehicle_actor)                   # add the vehicle to vehicle list 
 
+    #
     # define experiment parameters
+    #
     if len(sys.argv) > 2:
         experiment_execution = int(sys.argv[1])
     else:
         experiment_execution = 0
     
+    # experiment mode
     if 10 > experiment_execution > 0: 
         exp = Experiment()
         [aggressive, lawful, vehicles, pedestrians] = exp.execute_scenario(scenario=int(sys.argv[1]), dynamic_obstacles=int(sys.argv[2]))
@@ -92,35 +111,40 @@ def main():
         print("                                pedestrians: {}                                ".format(pedestrians) )
         print("-------------------------------------------------------------------------------"                     )
     
+    # normal mode 
     else:
         [aggressive, lawful, vehicles, pedestrians] = [0, 0, 0, 0]
         experiment = {'experiment': 0, 'aggresssive': aggressive, 'lawful': lawful, 'vehicles': vehicles, 'pedestrians': pedestrians}
 
-        print("-------- Running on normal mode with zero initial values for the siders --------\n")
+        print("-------- Running on normal mode with zero initial values for the sliders --------\n")
 
+    #
+    # spawn the specified number of dynamic obstacles 
+    #
     spawn(vehicle_list, walker_list, vehicles, pedestrians)
 
-    # plot vehicle's starting coordinate frame #
+    #
+    # plot vehicle's starting coordinate frame 
+    #
     origin = start_point 
     plot_axis(world, origin)
     
-    town = Town(world)
-
+    #
     # configure sensors 
+    #
     sensors = configure_sensor(vehicle_actor, vehicle_transform, blueprint, world, map, "ObstacleDetector")
 
+    #
     # add sensor to vehicle's configuration
+    #
     vehicle.add_sensor(sensors['obs'])
 
     # just wander in autopilot mode and collect data
     # vehicle.wander() 
 
-    #waypoints = map.generate_waypoints(1.0)
-    #waypoints = map.get_topology()
-    #for pair_wayoints in waypoints:
-    #   world.debug.draw_arrow(pair_wayoints[0].transform.location, pair_wayoints[1].transform.location, thickness=0.2, arrow_size=0.1, color=carla.Color(r=0, g=250, b=0), life_time=1000)
-
-    # generate random trajectory for the vehicle 
+    #
+    # initialize some publishers and subscribers  
+    #
     sub_done     = VehicleSubscriberDoneMQTT  (topic='done'              )
     sub_log      = VehicleSubscriberLogMQTT   (topic='log'               )
     sub_turn     = VehicleSubscriberTurnMQTT  (topic='turn_junction'     )
@@ -137,12 +161,11 @@ def main():
     pub_waypoint.publish({'value': ''                                           })
 
     cancel = Cancel()
+
     trajectory = Trajectory(world, map, vehicle_actor)
     interface  = Interface(world, map, vehicle_actor)
 
-    #pub_vel_conf.publish({'velocity': 0})
-    #pub_vel.publish({'velocity': 0})
-
+    # start a loop where the user gives the choices about the directions and the locations that wants to follow 
     while True:
 
         waypoints = []
@@ -262,7 +285,7 @@ def main():
         # follow trajectory and stop to obstacles and traffic lights
         try:
             behavior = Behavior(vehicle_actor, waypoints, trajectory, map, world, blueprint, vehicle_list, walker_list, experiment, exp)
-            behavior.follow_trajectory(world, vehicle_actor, vehicle.set_spectator, sensors['obs'].get_front_obstacle, sensors['obs'].set_front_obstacle, sensors['obs'].get_other_actor, 0)
+            behavior.follow_trajectory(world, vehicle_actor, vehicle.set_spectator, 0)
             
             vel = vehicle_actor.get_velocity()
             vector = [vel.x, vel.y, vel.z]
@@ -275,18 +298,16 @@ def main():
 
             start_waypoint = map.get_waypoint(vehicle_actor.get_location(), project_to_road=False, lane_type=carla.LaneType.Any)
             pub_waypoint.publish({'value': 'You have reached your destination! Define a new route to continue!'})
-            #pub_vel_conf.publish(msg)
-            #pub_vel.publish(msg)
 
         except IndexError:
             continue
 
-    ###########################
-    # Destroy actors and exit #
-    ###########################
+    #
+    # Destroy actors and exit 
+    #
     for actor in client.get_created_actors():
         actor.destroy()
-    print('done.')
+    print('Done!')
     world.tick()
     sys.exit()
 
